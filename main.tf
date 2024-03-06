b8ba0a7cc53da05d68e3f7b4b4dbe642abb12426
Kerwin Washington <kerwin.washington@gmail.com>
2024-03-06 09:26:49 -0600

    added backend

--- main.tf
+++ main.tf
@@ -1,35 +1,43 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.31.0"
     }
   }
+  backend "remote" {
+    organization = "Team-Helm"
+    hostname = "app.terraform.io"
+    workspaces {
+      prefix = "team2-"
+    }
+  }
+
 }
 
 provider "aws" {
   # Configuration options
-  profile = "<profileName>"
+  region = "us-east-1"
   default_tags {
     tags = {
-        Name = "Team-Helm"
+      Name = "Team-Helm"
     }
-  }  
+  }
 }
 
 resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
-#   tags = {
-#     Name = "washington"
-#   }
+  #   tags = {
+  #     Name = "washington"
+  #   }
 }
 
 resource "aws_subnet" "public" {
-  count = var.public_subnet_count
-  vpc_id     = aws_vpc.main.id
-  cidr_block = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
-  availability_zone = var.AZ[count.index]
+  count                   = var.public_subnet_count
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
+  availability_zone       = var.AZ[count.index]
   map_public_ip_on_launch = true
   tags = {
     Name = "Team-Helm-${count.index}"
@@ -44,7 +52,7 @@ resource "aws_internet_gateway" "gw" {
 }
 
 resource "aws_eip" "nat-eip" {
-  domain   = "vpc"
+  domain = "vpc"
 }
 
 resource "aws_route_table" "public" {
@@ -59,8 +67,8 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route_table_association" "public-rt" {
-  count = var.public_subnet_count
-  subnet_id = aws_subnet.public[count.index].id
+  count          = var.public_subnet_count
+  subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
 
